fd0738e986a5252d03de45147bbebe40e3e590d8
batel-shafir <batyushafir+1@gmail.com>
2020-05-18 10:35:13 +0300

    small changes

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -9,7 +9,7 @@ function About(props) {
 
   return (
     <div className="about">
-    <div className="about-content">
+      <div className="about-content">
         <div>My name is Batel Shafir and I'm a 
           <span className="color-1"> Frontend developer,</span>
         </div>
@@ -20,24 +20,24 @@ function About(props) {
           & <span className="color-1" > creative</span>.
         </div>
         <div>I believe that 
-          <span className="color-1"> "god is in the details"</span>
-        , which helps me to perform well as a Frontend Developer and basically in everything I do.</div> 
-        <div>
-        <span className="color-1">I have excellent interpersonal skills </span>
-         & high emotional intelligence.</div>
-         <div>I spend a lot of my spare time
-         <span className="color-1"> DIYing and Cooking</span>,
+          <span className="color-1"> "god is in the details",</span>
+          which helps me to perform well as a Frontend Developer and basically in 
+          everything I do.
+        </div> 
+        <div> I have excellent
+          <span className="color-1"> interpersonal skills </span> &
+          <span className="color-1"> high emotional intelligence </span>.
+        </div>
+        <div>I spend a lot of my spare time
+          <span className="color-1"> DIYing and Cooking</span>,
           take care of my plants, 
-         <span className="color-1"> cuddle my dog, </span>
-         and occasionally visit the beach that is 
-         <span className="color-1"> 5 minutes</span> walk from my flat.
+          <span className="color-1"> cuddle my dog, </span>
+          and occasionally visit the beach that is 
+          <span className="color-1"> 5 minutes</span> walk from my flat.
         </div>      
-    </div>
-    { <img className="my-img" src={mainImage}></img>}
-
-
-</div>
-    
+      </div>
+      <img className="my-img" src={mainImage}></img>
+    </div>  
   );
 }
 
