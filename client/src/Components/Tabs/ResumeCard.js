93c80e715bdd3edc6ee6a1af6f7e3ca432010cae
batel-shafir <batyushafir+1@gmail.com>
2020-06-22 00:14:11 +0300

    final changes

--- client/src/Components/Tabs/ResumeCard.js
+++ client/src/Components/Tabs/ResumeCard.js
@@ -6,9 +6,9 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
-import ProfessionalExperience from '../../assets/images/job.jpg';
+import ProfessionalExperience from '../../assets/images/job.png';
 import Education from '../../assets/images/school.jpg';
-import Volunteering from '../../assets/images/heart.jpg';
+import Volunteering from '../../assets/images/heart.png';
 import Military from '../../assets/images/army.jpg'
 
 import './Resume.scss'; 
